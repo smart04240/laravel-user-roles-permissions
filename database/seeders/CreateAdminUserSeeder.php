486bf04135099086bb13f40d77f81c52ca2674d1
smart04240 <smart04240@gmail.com>
2021-05-22 01:46:35 +1000

    add default roles

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -22,10 +22,10 @@ class CreateAdminUserSeeder extends Seeder
         	'password' => bcrypt('123456')
         ]);
   
-        $role = Role::create(['name' => 'Berichtswesen 1', 'guard_name' => 'berichtswesen1']);
-        $role = Role::create(['name' => 'Berichtswesen 2', 'guard_name' => 'berichtswesen2']);
-        $role = Role::create(['name' => 'Vertragswesen 1', 'guard_name' => 'vertragswesen1']);
-        $role = Role::create(['name' => 'Vertragswesen 2', 'guard_name' => 'vertragswesen2']);
+        $role = Role::create(['name' => 'Berichtswesen 1']);
+        $role = Role::create(['name' => 'Berichtswesen 2']);
+        $role = Role::create(['name' => 'Vertragswesen 1']);
+        $role = Role::create(['name' => 'Vertragswesen 2']);
    
         $permissions = Permission::pluck('id','id')->all();
   
