d1126ca374fd07d228f6d991fc82fc1ea4ae70b2
smart04240 <smart04240@gmail.com>
2021-05-22 01:34:13 +1000

    add default roles to seeder

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -22,7 +22,10 @@ class CreateAdminUserSeeder extends Seeder
         	'password' => bcrypt('123456')
         ]);
   
-        $role = Role::create(['name' => 'Admin']);
+        $role = Role::create(['name' => 'Berichtswesen 1', 'guard_name' => 'berichtswesen1']);
+        $role = Role::create(['name' => 'Berichtswesen 2', 'guard_name' => 'berichtswesen2']);
+        $role = Role::create(['name' => 'Vertragswesen 1', 'guard_name' => 'vertragswesen1']);
+        $role = Role::create(['name' => 'Vertragswesen 2', 'guard_name' => 'vertragswesen2']);
    
         $permissions = Permission::pluck('id','id')->all();
   
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel 8 User Roles and Permissions - ItSolutionStuff.com
+                    Laravel 8 User Roles and Permissions
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
